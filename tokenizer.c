ee33096e6279f0d32db5d67f9064a6c1475675dc
Crosby Thinkpad <crosbysayan2023@gmail.com>
2025-09-09 23:58:24 -0400

    Fixed bug with no Literal and added tokenizing

--- tokenizer.c
+++ tokenizer.c
@@ -1,20 +1,143 @@
-#include <stdio.h>
-
 #include "types.h"
-// Read file
-// Returns a linked list of all the tokens in the file
-void tokenizeFile() {
-    FILE* fd = fopen("./token-test.c", "r");
-    // After opening the file read the char by char with a buffer
-    char ch;
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
 
-    char buffer[1024];
-    char last_char = buffer[0];
-    while ((ch = fgetc(fd)) != EOF) {
-        // Single Char check/space or newline check
-        // if pass check the buffer before is a valid keyword
-        // else add the char to buffer
-    }
+bool is_breakpoint(char point) {
+  switch (point) {
+  case '(':
+  case ')':
+  case '{':
+  case '}':
+  case ';':
+  case ' ':
+  case '\n':
+  case '=':
+  case '\t':
+  case '\0':
+    return true;
+  default:
+    return false;
+  }
 }
 
-int main() { return 0; }
+TokenType fetchCharToken(char tokenChar) {
+  if (isdigit(tokenChar)) {
+    // number things
+    return LITERAL;
+  } else if (isalpha(tokenChar) || tokenChar == '_') {
+    // letter things
+    return IDEN;
+  } else {
+    return ILLEGAL;
+  }
+}
+
+void multiToken(char **tokenStart, TokenList *list) {
+  char *start = *tokenStart;
+  int pointer = 0;
+  TokenType tag = ILLEGAL;
+  bool flag = false;
+
+  while (start[pointer] != '\0' && !is_breakpoint(start[pointer])) {
+    char currentChar = start[pointer];
+    if (!flag) {
+      // Set TokenType tag
+      tag = fetchCharToken(currentChar);
+      flag = true;
+      pointer++;
+    } else {
+      // What is this char? does it match the current string of chars' type?
+      if (tag == ILLEGAL || fetchCharToken(currentChar) != tag) {
+        tag = ILLEGAL;
+      }
+      pointer++;
+    }
+  }
+
+  // Now with the full word determine if it is a keyword
+
+  if (pointer > 0) {
+    char tokenValue[pointer + 1];
+    strncpy(tokenValue, start, pointer);
+    tokenValue[pointer] = '\0';
+
+    // Check for keywords
+    if (tag == IDEN) {
+      if (strcmp(tokenValue, "int") == 0) {
+        tag = INT;
+      } else if (strcmp(tokenValue, "return") == 0) {
+        tag = RET;
+      }
+    }
+
+    add_token(list, create_token(tag, tokenValue));
+  }
+  *tokenStart += pointer;
+}
+//  Returns a linked list of all the tokens in the file
+void tokenize(char *buff, TokenList *list) {
+  char *currentPoint = buff;
+
+  while (*currentPoint != '\0') {
+    // Skip whitespace
+    if (*currentPoint == ' ' || *currentPoint == '\n' ||
+        *currentPoint == '\t') {
+      currentPoint++;
+      continue;
+    }
+
+    switch (*currentPoint) {
+    case '(':
+      add_token(list, create_token(LEFT_PAREN, "("));
+      currentPoint++;
+      break;
+    case ')':
+      add_token(list, create_token(RIGHT_PAREN, ")"));
+      currentPoint++;
+      break;
+    case '{':
+      add_token(list, create_token(LEFT_BRACK, "{"));
+      currentPoint++;
+      break;
+    case '}':
+      add_token(list, create_token(RIGHT_BRACK, "}"));
+      currentPoint++;
+      break;
+    case ';':
+      add_token(list, create_token(SEMI, ";"));
+      currentPoint++;
+      break;
+    case '=':
+      add_token(list, create_token(ASIGN, "="));
+      currentPoint++;
+      break;
+    default:
+      // Multi character token - multiToken will advance currentPoint
+      multiToken(&currentPoint, list);
+      break;
+    }
+  }
+}
+
+// int main() {
+//   const char *input = "int main() {\n return 0;\n }";
+//
+//   printf("Input: \"%s\"\n", input);
+//   printf("=====================================\n");
+//
+//   TokenList list;
+//   init_token_list(&list);
+//
+//   char *buffer = strdup(input);
+//   tokenize(buffer, &list);
+//   // Print the tokens
+//   print_token_list(&list);
+//
+//   // Cleanup
+//   free(buffer);
+//   free_token_list(&list);
+//
+//   return 0;
+// }
